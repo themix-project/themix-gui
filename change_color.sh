beecabfcf4ec05f8aff97ecc7514120167ba96b3
actionless <actionless.loveless@gmail.com>
2014-05-25 08:20:45 +0200

    ncc: gtk3: updated script

--- change_color.sh
+++ change_color.sh
@@ -7,22 +7,30 @@ test -z "$1" &&
 SRC_PATH=./
 DEST_PATH=~/.themes/ncc_current
 
-rm -r $DEST_PATH
-cp -r $SRC_PATH $DEST_PATH
-
-source ./colors/$1.sh
-
-     OLD_BG=$(sed -n '1p' < current_colors.txt)
-     OLD_FG=$(sed -n '2p' < current_colors.txt)
-    OLD_SEL=$(sed -n '3p' < current_colors.txt)
- OLD_TXT_BG=$(sed -n '4p' < current_colors.txt)
- OLD_TXT_FG=$(sed -n '5p' < current_colors.txt)
-OLD_MENU_BG=$(sed -n '6p' < current_colors.txt)
-OLD_MENU_FG=$(sed -n '7p' < current_colors.txt)
-
 replace () {
   grep -lZR $1 * | xargs -0 -n 1 sed -i -e 's/'"$2"'/'"$3"'/g'
 }
+
+(
+  rm -r $DEST_PATH ;
+  mkdir -p $DEST_PATH ;
+  cp -r \
+	  $SRC_PATH/index.theme \
+	  $SRC_PATH/gtk-2.0 \
+	  $SRC_PATH/gtk-3.0 \
+	  $SRC_PATH/openbox-3 \
+	$DEST_PATH
+) &&
+
+source ./colors/$1.sh &&
+
+     OLD_BG=$(sed -n '1p' < current_colors.txt) &&
+     OLD_FG=$(sed -n '2p' < current_colors.txt) &&
+    OLD_SEL=$(sed -n '3p' < current_colors.txt) &&
+ OLD_TXT_BG=$(sed -n '4p' < current_colors.txt) &&
+ OLD_TXT_FG=$(sed -n '5p' < current_colors.txt) &&
+OLD_MENU_BG=$(sed -n '6p' < current_colors.txt) &&
+OLD_MENU_FG=$(sed -n '7p' < current_colors.txt) &&
 
 cd $DEST_PATH &&
 replace $OLD_BG $OLD_BG $BG &&
