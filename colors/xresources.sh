427dd80bc0e320da81a1dc6c39aa1f1d9edbfe12
actionless <actionless.loveless@gmail.com>
2014-12-21 18:31:14 +0100

    feat(colors: xresources): added theme which following current terminal colorsheme defined in xresources

---
+++ colors/xresources.sh
@@ -0,0 +1,24 @@
+get_color () {
+	xrdb -query | grep "$1:" | cut -f2 | sed -r 's/#//' | head -n 1
+}
+color () {
+	get_color color$1
+}
+color_bg () {
+	get_color "*background"
+}
+color_fg () {
+	get_color "*foreground"
+}
+
+   NAME="follow xresources theme"
+     BG=$(color 15)
+     FG=$(color 0)
+ SEL_BG=$(color 2)
+ SEL_FG=$(color_fg)
+ TXT_BG=$(color 8)
+ TXT_FG=$(color 0)
+MENU_BG=$(color 0)
+MENU_FG=$(color 2)
+ BTN_BG=$(color 7)
+ BTN_FG=$(color_fg)
