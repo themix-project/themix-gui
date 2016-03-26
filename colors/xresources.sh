53dd330d072b8c69e0f46a50b6b3b2fdc82fbe07
actionless <actionless.loveless@gmail.com>
2016-03-26 02:14:08 +0100

    chore(themes): reorganize themes - 2

--- colors/xresources.sh
+++ colors/xresources.sh
@@ -14,11 +14,11 @@ color_fg () {
    NAME="follow xresources theme"
      BG=$(color 7)
      FG=$(color 0)
+ SEL_BG=$(color 4)
+ SEL_FG=$(color 15)
  TXT_BG=$(color_fg)
- TXT_FG=$(color 0)
- SEL_BG=$(color 1)
- SEL_FG=$TEXT_BG
+ TXT_FG=$(color_bg)
 MENU_BG=$(color_bg)
 MENU_FG=$BG
- BTN_BG=$(color 12)
- BTN_FG=$(color 0)
+ BTN_BG=$(color 8)
+ BTN_FG=$(color 15)
