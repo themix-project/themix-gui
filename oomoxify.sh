7b9be9e5eca534eb181ec04fe7354015220cdd10
actionless <actionless.loveless@gmail.com>
2016-05-04 03:09:10 +0200

    feat: add support for spotify

---
+++ oomoxify.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+set -ueo pipefail
+root="$(readlink -e $(dirname "$0"))"
+
+
+spotify_apps_path="/usr/share/spotify/Apps"
+backup_dir="${HOME}/.config/oomox/spotify_backup"
+
+
+print_usage() {
+	echo "usage: $0 [-s /path/to/spotify/Apps] PRESET_NAME_OR_PATH"
+	echo "examples:"
+	echo "       $0 monovedek"
+	echo "       $0 -o /opt/spotify/Apps ./colors/retro/twg"
+	exit 1
+}
+
+
+darker() {
+	hexinput=$(echo $1 | tr '[:lower:]' '[:upper:]')
+	light_delta=${2-10}
+
+    a=`echo $hexinput | cut -c-2`
+    b=`echo $hexinput | cut -c3-4`
+    c=`echo $hexinput | cut -c5-6`
+
+    r=`echo "ibase=16; $a - $light_delta" | bc`
+    g=`echo "ibase=16; $b - $light_delta" | bc`
+    b=`echo "ibase=16; $c - $light_delta" | bc`
+
+	echo $(echo "obase=16; $r" | bc ; echo "obase=16; $g" | bc ;echo "obase=16; $b" | bc) | tr -d ' '
+}
+
+
+while [[ $# > 0 ]]
+do
+	case ${1} in
+		-s|--spotify-apps-path)
+			spotify_apps_path="${2}"
+			shift
+		;;
+		*)
+			if [[ "${1}" == -* ]] || [[ ${THEME-} ]]; then
+				echo "unknown option ${1}"
+				print_usage
+				exit 2
+			fi
+			THEME="${1}"
+		;;
+	esac
+	shift
+done
+
+if [[ -z "${THEME:-}" ]] ; then
+	print_usage
+fi
+
+if [[ ${THEME} == */* ]] || [[ ${THEME} == *.* ]] ; then
+	source "$THEME"
+	THEME=$(basename ${THEME})
+else
+	source "${root}/colors/$THEME"
+fi
+
+
+main_bg="${BG}"
+area_bg="${TXT_BG}"
+selected_row_bg="$(darker ${BG})"
+
+sidebar_fg="${FG}"
+main_fg="${FG}"
+accent_fg="$(darker ${BTN_BG} 30)"
+
+hover_text="${SEL_BG}"
+active_selection_color="${SEL_BG}"
+inactive_selection_color="$(darker ${SEL_BG})"
+
+
+tmp_dir="$(mktemp -d)"
+output_dir="$(mktemp -d)"
+function post_clean_up {
+	rm -r "${tmp_dir}" || true
+	rm -r "${output_dir}" || true
+}
+trap post_clean_up EXIT SIGHUP SIGINT SIGTERM
+
+if [[ ! -d "${backup_dir}" ]] ; then
+	mkdir "${backup_dir}"
+	cp -prf "${spotify_apps_path}"/*.spa "${backup_dir}/"
+fi
+
+cd "${root}"
+for file in $(ls "${backup_dir}"/*.spa) ; do
+	filename="$(basename "${file}")"
+	echo "${filename}"
+	cp "${file}" "${tmp_dir}/"
+	cd "${tmp_dir}"
+	unzip "./${filename}" > /dev/null
+	if [[ -d ./css/ ]] && [[ -f ./css/style.css ]] ; then
+		for css in $(ls ./css/*.css); do
+			sed -i \
+				-e "s/282828/oomox_main_bg/g" \
+				-e "s/181818/oomox_area_bg/g" \
+				-e "s/333333/oomox_selected_row_bg/g" \
+				-e "s/ffffff/oomox_accent_fg/gI" \
+				-e "s/fcfcfc/oomox_hover_text/gI" \
+				-e "s/1ed760/oomox_active_selection_color/gI" \
+				-e "s/1db954/oomox_inactive_selection_color/gI" \
+				-e "s/a0a0a0/oomox_main_fg/gI" \
+				-e "s/adafb2/oomox_sidebar_fg/gI" \
+				"${css}"
+			sed -i \
+				-e "s/oomox_main_bg/${main_bg}/g" \
+				-e "s/oomox_area_bg/${area_bg}/g" \
+				-e "s/oomox_selected_row_bg/${selected_row_bg}/g" \
+				-e "s/oomox_accent_fg/${accent_fg}/gI" \
+				-e "s/oomox_hover_text/${hover_text}/gI" \
+				-e "s/oomox_active_selection_color/${active_selection_color}/gI" \
+				-e "s/oomox_inactive_selection_color/${inactive_selection_color}/gI" \
+				-e "s/oomox_main_fg/${main_fg}/gI" \
+				-e "s/oomox_sidebar_fg/${sidebar_fg}/gI" \
+				"${css}"
+			zip "./${filename}" "${css}" > /dev/null
+		done
+		cd "${tmp_dir}"
+		mv "./${filename}" "${output_dir}/"
+	fi
+	rm "${tmp_dir}/"* -r
+done
+
+sudo cp "${output_dir}/"* "${spotify_apps_path}"/
