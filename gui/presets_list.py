f2dcc003413d189e7a567c1799e1353102a76585
actionless <actionless.loveless@gmail.com>
2016-07-06 00:08:01 +0200

    feat(gui: presets_list): distinguish default themes from user-added ones with the same name

--- gui/presets_list.py
+++ gui/presets_list.py
@@ -12,13 +12,17 @@ class ThemePresetsList(Gtk.Box):
     treeiter = None
     preset_select_callback = None
 
+    DISPLAY_NAME = 0
+    THEME_NAME = 1
+    THEME_PATH = 2
+
     def on_preset_select(self, widget):
         list_index = widget.get_cursor()[0].to_string()
         selected_preset = list(
             self.treestore[list_index]
         )
-        self.current_theme = selected_preset[0]
-        self.current_preset_path = selected_preset[1]
+        self.current_theme = selected_preset[self.THEME_NAME]
+        self.current_preset_path = selected_preset[self.THEME_PATH]
         self.preset_select_callback(
             self.current_theme, self.current_preset_path
         )
@@ -26,42 +30,55 @@ class ThemePresetsList(Gtk.Box):
             Gtk.TreePath.new_from_string(list_index)
         )
 
-    def add_preset(self, preset_name, preset_path):
-        self.treestore.append(None, (preset_name, preset_path))
+    def add_preset(self, preset_name, preset_path, display_name=None):
+        if not display_name:
+            display_name = preset_name
+        self.treestore.append(None, (display_name, preset_name, preset_path))
+
+    def focus_previous(self):
+        treepath = self.treeview.get_cursor()[0]
+        treepath.prev()
+        self.treeview.set_cursor(treepath)
+
+    def update_current_preset_display_name(self, new_name):
+        self.treestore[self.treeiter][self.DISPLAY_NAME] = new_name
+
+    def update_current_preset_name(self, new_name):
+        self.treestore[self.treeiter][self.THEME_NAME] = new_name
 
     def update_current_preset_path(self, new_path):
-        self.treestore[self.treeiter][1] = new_path
+        self.treestore[self.treeiter][self.THEME_PATH] = new_path
 
     def __init__(self, preset_select_callback):
         super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self.preset_select_callback = preset_select_callback
         self.presets = get_presets()
 
-        self.treestore = Gtk.TreeStore(str, str)
+        self.treestore = Gtk.TreeStore(str, str, str)
         for preset_dir, preset_list in self.presets.items():
             sorted_preset_list = sorted(preset_list, key=lambda x: x['name'])
             piter = self.treestore.append(
                 None,
-                (sorted_preset_list[0]['name'], sorted_preset_list[0]['path'])
+                (preset_dir, sorted_preset_list[0]['name'], sorted_preset_list[0]['path'])
             )
             for preset in sorted_preset_list[1:]:
                 self.treestore.append(
                     piter,
-                    (preset['name'], preset['path'])
+                    (preset['name'], preset['name'], preset['path'])
                 )
         self.treestore.set_sort_column_id(0, Gtk.SortType.ASCENDING)
 
-        treeview = Gtk.TreeView(model=self.treestore, headers_visible=False)
-        treeview.connect("cursor_changed", self.on_preset_select)
+        self.treeview = Gtk.TreeView(model=self.treestore, headers_visible=False)
+        self.treeview.connect("cursor_changed", self.on_preset_select)
 
         column = Gtk.TreeViewColumn(
             cell_renderer=Gtk.CellRendererText(), text=0
         )
-        treeview.append_column(column)
+        self.treeview.append_column(column)
 
         scrolled = Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        scrolled.add(treeview)
+        scrolled.add(self.treeview)
 
         presets_list_label = Gtk.Label()
         presets_list_label.set_text("Presets:")
