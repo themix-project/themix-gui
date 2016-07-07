55191ce6d6d338ee8725f9820cb8e07660dbee33
actionless <actionless.loveless@gmail.com>
2016-07-07 22:52:19 +0200

    feat(oomoxify): add missing color for hover over selected item - 2

--- oomoxify.sh
+++ oomoxify.sh
@@ -143,6 +143,7 @@ for file in $(ls "${backup_dir}"/*.spa | grep -v messages) ; do
 				-e "s/1ed660/oomox_active_selection_color/gI" \
 				-e "s/1db954/oomox_inactive_selection_color/gI" \
 				-e "s/1df269/oomox_hover_selection_color/gI" \
+				-e "s/1bd85e/oomox_hover_selection_color/gI" \
 				-e "s/a0a0a0/oomox_main_fg/gI" \
 				-e "s/adafb2/oomox_sidebar_fg/gI" \
 				"${css}"
