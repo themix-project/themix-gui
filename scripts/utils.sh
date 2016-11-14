795932a776a717653d70e7f72fcba4f19fc8e91b
Dustin Falgout <dustin@falgout.us>
2016-11-14 16:22:11 -0600

    update utils.sh script

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -56,7 +56,7 @@ update_changes_file() {
 	LAST_PATCH="${LAST_STABLE_RELEASE##*.}"
 
 	case "${PWD##*/}" in
-		Numix)
+		numix-gtk-theme)
 			NEXT_PATCH=$(($LAST_PATCH + 1))
 
 			NEXT_STABLE_RELEASE="${LAST_MAJOR_MINOR}.${NEXT_PATCH}"
