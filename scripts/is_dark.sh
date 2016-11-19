8921468a6b6d160da1883753988dc7df6b401ded
actionless <actionless.loveless@gmail.com>
2016-11-19 07:25:56 +0100

    fix(spotify): remove temporary fg workaround (using hardcoded color)

---
+++ scripts/is_dark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -x
+
+script_dir=$(readlink -e $(dirname "${0}"))
+
+is_dark() {
+	hexinput=$(echo $1 | tr '[:lower:]' '[:upper:]')
+	half_darker=$("${script_dir}/darker.sh" "${hexinput}" 88)
+	if [[ "${half_darker}" = "000000" ]] ; then
+		return 0;
+	else
+		return 1;
+	fi
+}
+
+is_dark $@
