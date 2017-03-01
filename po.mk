19cfb10ea3874a96e70a746831244ce14de1eb74
Colomban Wendling <cwendling@hypra.fr>
2017-03-01 16:26:54 +0100

    feat(i18n): add translation infrastructure

---
+++ po.mk
@@ -0,0 +1,52 @@
+#!/usr/bin/make -f
+
+DOMAIN = oomox
+PODIR = po
+SOURCES = $(wildcard gui/*.py)
+LOCALEDIR = ./locale
+
+XGETTEXT ?= xgettext --package-name=$(DOMAIN) --foreign-user
+MSGINIT ?= msginit
+MSGMERGE ?= msgmerge
+MSGFMT ?= msgfmt
+RM ?= rm -f
+INSTALL ?= install
+MKDIR_P ?= mkdir -p
+
+POTFILE = $(PODIR)/$(DOMAIN).pot
+ALL_PO = $(wildcard $(PODIR)/*.po)
+ALL_MO = $(ALL_PO:.po=.mo)
+
+all: install
+
+update-pot: $(POTFILE)
+update-po: $(ALL_PO)
+
+$(POTFILE): $(SOURCES)
+	test -d $(PODIR) || $(MKDIR_P) $(PODIR)
+	$(XGETTEXT) -o $@ $^
+
+$(PODIR)/%.po: $(POTFILE)
+	if test -f $@; then \
+		$(MSGMERGE) -U $@ $(POTFILE); \
+	else \
+		$(MSGINIT) -o $@ -i $(POTFILE); \
+	fi
+
+$(PODIR)/%.mo: $(PODIR)/%.po
+	$(MSGFMT) -o $@ $^
+
+install: $(ALL_MO)
+	for f in $^; do \
+		l="$$(basename "$$f" .mo)"; \
+		d="$(LOCALEDIR)/$$l/LC_MESSAGES"; \
+		$(MKDIR_P) "$$d" || exit; \
+		$(INSTALL) -m 644 "$$f" "$$d/$(DOMAIN).mo"; \
+	done
+
+check: $(ALL_PO)
+	$(MSGFMT) --check --check-accelerators=_ --statistics $^
+
+clean:
+	$(RM) $(POTFILE)
+	$(RM) $(PODIR)/*.mo
