157e65b630ef901aef77f92af390be9afe82305b
actionless <actionless.loveless@gmail.com>
2017-12-22 22:46:13 +0100

    chore(maintenance_scripts): add one for showing submodules' status and log

---
+++ maintenance_scripts/show_submodule_diff.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+for submodule in $(git config --file .gitmodules --get-regexp path | awk '{ print $2 }') ; do
+	hashes=$(git diff | grep ${submodule} -A 3 | grep -e "^.Subproject" | cut -d ' ' -f3 | cut -d- -f1 | paste -sd '%' -)
+	if [[ ! -z "${hashes}" ]] ; then
+		echo
+		echo SUBMODULE ${submodule}:
+		echo
+		hash1=$(cut -d% -f1 <<< ${hashes})
+		hash2=$(cut -d% -f2 <<< ${hashes})
+		if [[ ${hash1} = ${hash2} ]] ; then
+			git -C ${submodule}/ status
+		else
+			echo ${hashes}
+			git -C ${submodule}/ log --oneline ${hash1}..${hash2}
+		fi
+	fi
+done
