265771abc896700049e59e369275e0d756c80171
actionless <actionless.loveless@gmail.com>
2017-12-31 21:19:48 +0100

    chore(submodules: oomoxify, gtk-theme): force C numeric locale, fixes #113

--- packaging/ubuntu/docker_ubuntu_package.sh
+++ packaging/ubuntu/docker_ubuntu_package.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 here=$(pwd)
 srcdir="$(readlink -e $(dirname ${0})/../..)"
 
-cp ./Dockerfile ${srcdir}/
+cp ${srcdir}/packaging/ubuntu/Dockerfile ${srcdir}/
 (docker images | grep oomox_ubuntu_zesty_build_image) || docker build -t oomox_ubuntu_zesty_build_image ${srcdir}
 docker run -t --name oomox_ubuntu_zesty_build -v ${srcdir}:/opt/oomox oomox_ubuntu_zesty_build_image
 docker cp oomox_ubuntu_zesty_build:/opt/oomox/ubuntu_package/oomox.deb ${here}
