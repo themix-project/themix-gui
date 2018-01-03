1fec5210d2795366494f8ac8f6dad3847f3b4761
actionless <actionless.loveless@gmail.com>
2018-01-03 15:01:58 +0100

    style(gui: preview: terminal): pylint

--- oomox_gui/preview_terminal.py
+++ oomox_gui/preview_terminal.py
@@ -28,37 +28,37 @@ class TerminalThemePreview(Gtk.Grid):
         self.set_margin_left(WIDGET_SPACING)
         self.set_margin_right(WIDGET_SPACING)
 
-        self.bg = Gtk.Grid(row_spacing=6, column_spacing=6)
-        self.bg.set_margin_top(WIDGET_SPACING/2)
-        self.bg.set_margin_bottom(WIDGET_SPACING)
+        self.background = Gtk.Grid(row_spacing=6, column_spacing=6)
+        self.background.set_margin_top(WIDGET_SPACING/2)
+        self.background.set_margin_bottom(WIDGET_SPACING)
 
         self.terminal_widgets = {}
-        tw = self.terminal_widgets
+        twi = self.terminal_widgets
 
-        tw["normal"] = Gtk.Label()
-        tw["normal"].set_markup("<tt>{}</tt>".format(_("terminal colors:")))
-        self.bg.attach(tw["normal"], 1, 1, 2, 1)
-        previous_row = tw["normal"]
+        twi["normal"] = Gtk.Label()
+        twi["normal"].set_markup("<tt>{}</tt>".format(_("terminal colors:")))
+        self.background.attach(twi["normal"], 1, 1, 2, 1)
+        previous_row = twi["normal"]
         previous_row.set_margin_left(self.LEFT_MARGIN)
         for color_row in self.COLOR_ROWS:
             color_name, normal_id, highlight_id = color_row
             key1 = "color{}".format(normal_id)
             key2 = "color{}".format(highlight_id)
-            tw[key1] = Gtk.Label()
-            tw[key2] = Gtk.Label()
-            tw[key1].set_markup("<tt>{}</tt>".format(color_name))
-            tw[key2].set_markup("<tt>{}</tt>".format(color_name))
-            self.bg.attach_next_to(
-                tw[key1], previous_row,
+            twi[key1] = Gtk.Label()
+            twi[key2] = Gtk.Label()
+            twi[key1].set_markup("<tt>{}</tt>".format(color_name))
+            twi[key2].set_markup("<tt>{}</tt>".format(color_name))
+            self.background.attach_next_to(
+                twi[key1], previous_row,
                 Gtk.PositionType.BOTTOM, 1, 1
             )
-            self.bg.attach_next_to(
-                tw[key2], tw[key1],
+            self.background.attach_next_to(
+                twi[key2], twi[key1],
                 Gtk.PositionType.RIGHT, 1, 1
             )
-            previous_row = tw[key1]
+            previous_row = twi[key1]
             previous_row.set_margin_left(self.LEFT_MARGIN)
-        self.attach(self.bg, 1, 1, 1, 1)
+        self.attach(self.background, 1, 1, 1, 1)
 
     def update_preview(self, colorscheme):
         term_colorscheme = generate_xrdb_theme_from_oomox(colorscheme)
@@ -73,7 +73,7 @@ class TerminalThemePreview(Gtk.Grid):
         )
         self.override_background_color(Gtk.StateType.NORMAL, term_bg)
         for color_row in self.COLOR_ROWS:
-            color_name, normal_id, highlight_id = color_row
+            _color_name, normal_id, highlight_id = color_row
             key1 = "color{}".format(normal_id)
             key2 = "color{}".format(highlight_id)
             self.terminal_widgets[key1].override_color(
