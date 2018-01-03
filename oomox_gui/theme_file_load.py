bf36614a68aa3bc5451e8f82fdda5098fd0add4e
actionless <actionless.loveless@gmail.com>
2018-01-03 18:16:36 +0100

    refactor(gui: theme_file: load): pylint

--- oomox_gui/theme_file_load.py
+++ oomox_gui/theme_file_load.py
@@ -38,14 +38,45 @@ def bash_preprocess(preset_path):
     return colorscheme
 
 
+def parse_theme_value(theme_value, colorscheme):
+    result_value = colorscheme.get(theme_value['key'])
+    fallback_key = theme_value.get('fallback_key')
+    fallback_value = theme_value.get('fallback_value')
+
+    if result_value is None and (fallback_key or fallback_value is not None):
+        if fallback_value is not None:
+            result_value = fallback_value
+        else:
+            result_value = colorscheme[fallback_key]
+
+    value_type = theme_value['type']
+    if value_type == 'bool':
+        if isinstance(result_value, str):
+            result_value = str_to_bool(result_value)
+    elif value_type == 'int':
+        result_value = int(result_value)
+    elif value_type == 'float':
+        result_value = float(result_value)
+    elif value_type == 'options':
+        available_options = [option['value'] for option in theme_value['options']]
+        if result_value not in available_options:
+            if fallback_value in available_options:
+                result_value = fallback_value
+            else:
+                result_value = available_options[0]
+
+    return result_value
+
+
 def read_colorscheme_from_path(preset_path):
     theme_keys = [item['key'] for item in theme_model if 'key' in item]
 
     # @TODO: remove legacy stuff (using bash logic inside the themes)
     theme_keys.append('NOGUI')
+
     colorscheme = {}
-    with open(preset_path) as f:
-        for line in f.readlines():
+    with open(preset_path) as file_object:
+        for line in file_object.readlines():
             parsed_line = line.strip().split('=')
             key = parsed_line[0]
             try:
@@ -56,40 +87,14 @@ def read_colorscheme_from_path(preset_path):
             except IndexError:
                 pass
 
-    # migration workaround:
+    # @TODO: remove migration workaround #2:
     if colorscheme.get('NOGUI'):
         colorscheme = bash_preprocess(preset_path)
 
-    for theme_value in theme_model:
-        key = theme_value.get('key')
+    for theme_model_item in theme_model:
+        key = theme_model_item.get('key')
         if not key:
             continue
-        fallback_key = theme_value.get('fallback_key')
-        fallback_value = theme_value.get('fallback_value')
-        value = colorscheme.get(key)
-        if value is None and (fallback_key or fallback_value is not None):
-            if fallback_value is not None:
-                value = colorscheme[key] = fallback_value
-            else:
-                value = colorscheme[key] = colorscheme[fallback_key]
-
-        if value is None and fallback_value is False:
-            colorscheme[key] = FALLBACK_COLOR
-
-        value_type = theme_value['type']
-        if value_type == 'bool':
-            if isinstance(value, str):
-                colorscheme[key] = str_to_bool(value)
-        elif value_type == 'int':
-            colorscheme[key] = int(value)
-        elif value_type == 'float':
-            colorscheme[key] = float(value)
-        elif value_type == 'options':
-            available_options = [option['value'] for option in theme_value['options']]
-            if value not in available_options:
-                if fallback_value in available_options:
-                    colorscheme[key] = fallback_value
-                else:
-                    colorscheme[key] = available_options[0]
+        colorscheme[key] = parse_theme_value(theme_model_item, colorscheme)
 
     return colorscheme
