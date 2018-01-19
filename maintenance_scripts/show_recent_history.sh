bd3d98176885147b6e3d6858aaa6c6f39fceadfc
actionless <actionless.loveless@gmail.com>
2018-01-19 22:11:48 +0100

    feat(maintenance scripts: show recent history): add '-c' option for clean output (filter out not important commits)

--- maintenance_scripts/show_recent_history.sh
+++ maintenance_scripts/show_recent_history.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
+filter=cat
+if [[ "${1:-}" = '-c' ]] ; then
+	filter="grep -v -i -e style -e travis -e docker -e chore -e test -e revert -e screensh -e merge"
+	shift
+fi
+
 git log \
 	--pretty=tformat:"%Cred%D%Creset %ad %Cgreen%h %Cblue%an %Creset%s" \
 	--date='format:%Y-%m-%d' \
 	--color=always \
-	$(git tag | sort -V | tail -n1)~1.. \
-	$@
+	$(git tag | grep -v gtk | sort -V | tail -n1)~1.. \
+	$@ \
+| $filter
