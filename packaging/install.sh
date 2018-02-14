97e52296124d3df450467ba179d8944dc236e88a
actionless <actionless.loveless@gmail.com>
2018-02-14 01:44:06 +0100

    fix(packaging: install.sh): quote paths

--- packaging/install.sh
+++ packaging/install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
+#set -x
 set -ueo pipefail
 
 # @TODO: mb rewrite it as a Makefile
 
-srcdir=$(readlink -e $1)
-pkgdir=$(readlink -e $2)
+srcdir=$(readlink -e "$1")
+#pkgdir=$(readlink -e "$2")
+pkgdir="${2}"
 
-if [[ -d ${pkgdir}/opt/oomox ]] ; then
-	rm -r ${pkgdir}/opt/oomox
+if [[ -d "${pkgdir}/opt/oomox" ]] ; then
+	rm -r "${pkgdir}/opt/oomox"
 fi
-mkdir -p ${pkgdir}/opt/oomox
+mkdir -p "${pkgdir}/opt/oomox"
 
-cd ${srcdir}
+cd "${srcdir}"
 cp -prf \
 	./CREDITS \
 	./LICENSE \
@@ -23,28 +25,28 @@ cp -prf \
 	./po \
 	./po.mk \
 	./terminal_templates \
-		${pkgdir}/opt/oomox
-rm -rf ${pkgdir}/opt/oomox/plugins/theme_materia/materia-theme/.git*
-rm -rf ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/.git*
-rm -rf ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/.editorconfig
-rm -rf ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/.*.yml
-rm -r ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/{D,d}ocker*
-rm -r ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/maintenance*
-rm -r ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/screenshot*
-rm -r ${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/test*
-rm -rf ${pkgdir}/opt/oomox/plugins/icons_archdroid/archdroid-icon-theme/.git*
-rm -rf ${pkgdir}/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/.git*
-rm -rf ${pkgdir}/opt/oomox/plugins/oomoxify/.git*
+		"${pkgdir}/opt/oomox"
+rm -rf "${pkgdir}/opt/oomox/plugins/theme_materia/materia-theme/".git*
+rm -rf "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/".git*
+rm -rf "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/".editorconfig
+rm -rf "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/".*.yml
+rm -r "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/"{D,d}ocker*
+rm -r "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/"maintenance*
+rm -r "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/"screenshot*
+rm -r "${pkgdir}/opt/oomox/plugins/theme_oomox/gtk-theme/"test*
+rm -rf "${pkgdir}/opt/oomox/plugins/icons_archdroid/archdroid-icon-theme/".git*
+rm -rf "${pkgdir}/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/".git*
+rm -rf "${pkgdir}/opt/oomox/plugins/oomoxify/".git*
 
-cd ${pkgdir}/opt/oomox/
+cd "${pkgdir}/opt/oomox/"
 # will update ./po and produce ./locale dir:
 make -f po.mk install
-rm ${pkgdir}/opt/oomox/po.mk
+rm "${pkgdir}/opt/oomox/po.mk"
 
-mkdir -p ${pkgdir}/usr/bin/
-cp ${srcdir}/packaging/bin/* ${pkgdir}/usr/bin/
+mkdir -p "${pkgdir}/usr/bin/"
+cp "${srcdir}/packaging/bin/"* "${pkgdir}/usr/bin/"
 
-mkdir -p ${pkgdir}/usr/share/applications/
-cp ${srcdir}/packaging/oomox.desktop ${pkgdir}/usr/share/applications/
+mkdir -p "${pkgdir}/usr/share/applications/"
+cp "${srcdir}/packaging/oomox.desktop" "${pkgdir}/usr/share/applications/"
 
 exit 0
