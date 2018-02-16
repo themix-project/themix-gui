fcacda21642817561dbb8e86313da14e8ce26691
actionless <actionless.loveless@gmail.com>
2018-02-16 17:29:01 +0100

    fix(packaging: flatpak): typo in project name

--- maintenance_scripts/tag_release.sh
+++ maintenance_scripts/tag_release.sh
@@ -15,6 +15,6 @@ fi
 sed -i -e "s/pkgver=.*/pkgver=${new_version}/g" packaging/arch/PKGBUILD
 sed -i -e "s/pkgrel=.*/pkgrel=1/g" packaging/arch/PKGBUILD
 sed -i -e "s/Version: .*-1~actionless~zesty/Version: ${new_version}-1~actionless~zesty/g" packaging/ubuntu/control
-sed -i -e 's/"tag": ".*"/"tag": "'"${new_version}"'"/g' packaging/flatpak/com.github.actionless.json
+sed -i -e 's/"tag": ".*"/"tag": "'"${new_version}"'"/g' packaging/flatpak/com.github.actionless.oomox.json
 git commit -am "chore: bump version to ${new_version}"
 git tag -a "${new_version}"
