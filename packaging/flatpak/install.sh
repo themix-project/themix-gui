9a040b670c1f4af101350461a07d4bebfa548785
actionless <actionless.loveless@gmail.com>
2018-02-26 16:31:55 +0100

    chore(packaging: flatpak): update imagemagick dep and use different separator in sed

--- packaging/flatpak/install.sh
+++ packaging/flatpak/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-SCRIPT_DIR=$(readlink -e $(dirname "${0}"))
+SCRIPT_DIR=$(readlink -e "$(dirname "${0}")")
 
 cd "${SCRIPT_DIR}"/../..
 
 rm -r ./plugins/{oomoxify,theme_materia}
-sed -i -e 's/\\/opt\\/oomox\\//\\/app\\/opt\\/oomox\\//g' ./packaging/bin/*
+sed -i -e 's|/opt/oomox/|/app/opt/oomox/|g' ./packaging/bin/*
 ./packaging/install.sh ./ /app/
 python3 -O -m compileall /app/opt/oomox/oomox_gui
 
