d1ae6fbd7205eda7a301bc4603c6077e6fd61972
actionless <actionless.loveless@gmail.com>
2018-05-03 18:20:22 +0200

    fix(run_test): kill xvfb in case of failure; be more verbose

--- maintenance_scripts/run_test.sh
+++ maintenance_scripts/run_test.sh
@@ -2,13 +2,22 @@
 set -ueo pipefail
 
 Xvfb :99 -ac -screen 0 1920x1080x16 -nolisten tcp &
+xvfb_pid=$!
+
+clean_up() {
+	kill ${xvfb_pid}
+}
+trap clean_up EXIT SIGHUP SIGINT SIGTERM
+
 echo '== Started Xvfb'
 echo '== Running on system python'
 python3 --version
 export DISPLAY=:99
 sleep 3
 
+echo -e "\n== Checking main app:"
 pylint oomox_gui
-echo -n plugins/*/oomox_plugin.py | xargs -d ' ' -n 1 pylint
-
-killall Xvfb
+for plugin in ./plugins/*/oomox_plugin.py ; do
+	echo -e "\n== Checking plugin ${plugin}:"
+	pylint $plugin
+done
