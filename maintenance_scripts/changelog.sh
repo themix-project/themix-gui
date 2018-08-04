7f7f89947e19953bfc892ff1ec42dcd4299d4426
actionless <actionless.loveless@gmail.com>
2018-08-04 04:09:14 +0200

    chore(maintenance_scripts): add one for generating changelog

---
+++ maintenance_scripts/changelog.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This file is licensed under GPLv3, see https://www.gnu.org/licenses/
+
+set -euo pipefail
+IFS=$'\n\t'
+
+for tag in $(git tag | sort -V) ; do
+	echo -e '\n'
+	git tag -n --format '%(taggerdate)' $tag
+	echo '-------------------------------------------------'
+	git tag -n9 $tag
+done
