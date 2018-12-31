743c7093e44f088352ab91e90734d44ac2d2b95a
actionless <actionless.loveless@gmail.com>
2018-12-31 02:22:50 +0100

    fix(maintenance_scripts): add missing shellcheck script

---
+++ maintenance_scripts/shellcheck.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+TEST_DIR=$(readlink -e "$(dirname "${0}")")
+SCRIPT_DIR="$(readlink -e "${TEST_DIR}"/..)"
+
+(
+	cd "${SCRIPT_DIR}"
+	# shellcheck disable=SC2046
+	shellcheck $(find . \
+		-name '*.sh' \
+		-not -path './plugins/icons_archdroid/archdroid-icon-theme/*' \
+		-not -path './plugins/icons_gnomecolors/gnome-colors-icon-theme/*' \
+		-not -path './plugins/oomoxify/*' \
+		-not -path './plugins/theme_arc/arc-theme/*' \
+		-not -path './plugins/theme_materia/materia-theme/*' \
+		-not -path './plugins/theme_oomox/gtk-theme/*' \
+	)
+)
+
+echo '$$ shellcheck passed $$'
+exit 0
