3ce3c339272db58f93a8f1c794f6f949bb6127e1
actionless <actionless.loveless@gmail.com>
2019-01-02 06:15:18 +0100

    feat(shortcuts_popup: presets_list): add help for F5 - refresh list

--- maintenance_scripts/git-diff.sh
+++ maintenance_scripts/git-diff.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git -c 'color.status=always' status | grep -v -e colors/ -e dumpster/ -e git-diff
+git -c 'color.status=always' status | grep -v -e colors/ -e dumpster/ -e git-diff -e '\[31mgtk-theme\[m' -e env35/
