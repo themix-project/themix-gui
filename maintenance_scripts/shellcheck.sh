ef3a69dc1aed10125f8a0df8d116763b2f7ea39c
actionless <actionless.loveless@gmail.com>
2019-01-18 20:48:18 +0100

    chore(shellcheck): exclude papirus submodule from check

--- maintenance_scripts/shellcheck.sh
+++ maintenance_scripts/shellcheck.sh
@@ -12,6 +12,7 @@ SCRIPT_DIR="$(readlink -e "${TEST_DIR}"/..)"
 		-name '*.sh' \
 		-not -path './plugins/icons_archdroid/archdroid-icon-theme/*' \
 		-not -path './plugins/icons_gnomecolors/gnome-colors-icon-theme/*' \
+		-not -path './plugins/icons_papirus/papirus-icon-theme/*' \
 		-not -path './plugins/base16/base16_mirror/*.tmp/*' \
 		-not -path './plugins/oomoxify/*' \
 		-not -path './plugins/theme_arc/arc-theme/*' \
