70509a27ad17e3ad45a4095810db6013614fc360
actionless <actionless.loveless@gmail.com>
2020-03-17 21:01:28 +0100

    chore(maintenance-scripts: tag-release): add new arch packages

--- maintenance_scripts/tag_release.sh
+++ maintenance_scripts/tag_release.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 
 new_version=$1
-aur_dev_repo_dir=~/build/oomox-git
+aur_dev_repo_root=~/build/
 
 
 if [[ $(git status --porcelain 2>/dev/null| grep -c "^ [MD]") -gt 0 ]] ; then
@@ -37,8 +37,19 @@ echo "**    Updating version...    **"
 echo "*******************************"
 echo
 
-sed -i -e "s/pkgver=.*/pkgver=${new_version}/g" packaging/arch/PKGBUILD
-sed -i -e "s/pkgrel=.*/pkgrel=1/g" packaging/arch/PKGBUILD
+pkgbuilds=(
+		packaging/arch/PKGBUILD_full
+		packaging/arch/PKGBUILD_full_git
+		packaging/arch/PKGBUILD_gui
+		packaging/arch/PKGBUILD_import_image
+		packaging/arch/PKGBUILD_migration
+		packaging/arch/PKGBUILD_old
+)
+for pkgbuild_path in "${pkgbuilds[@]}"; do
+	sed -i -e "s/pkgver=.*/pkgver=${new_version}/g" "$pkgbuild_path"
+	sed -i -e "s/pkgrel=.*/pkgrel=1/g" "$pkgbuild_path"
+done
+
 sed -i -e "s/^Version: .*/Version: ${new_version}-1~themixproject~zesty/" packaging/ubuntu/control
 sed -i -e "s/^Version: .*/Version: ${new_version}-1~themixproject~cosmic/" packaging/ubuntu/control_1810
 sed -i -e 's/"tag": ".*"/"tag": "'"${new_version}"'"/g' packaging/flatpak/com.github.themix_project.Oomox.json
@@ -52,7 +63,12 @@ echo -n "> "
 read -r answer
 echo
 if [[ "${answer}" = "y" ]] ; then
-	git add packaging/arch/PKGBUILD packaging/ubuntu/control packaging/ubuntu/control_1810 packaging/flatpak/com.github.themix_project.Oomox.json packaging/com.github.themix_project.Oomox.appdata.xml
+	git add \
+		"${pkgbuilds[@]}" \
+		packaging/ubuntu/control \
+		packaging/ubuntu/control_1810 \
+		packaging/flatpak/com.github.themix_project.Oomox.json \
+		packaging/com.github.themix_project.Oomox.appdata.xml
 	git commit -m "chore: bump version to ${new_version}" || true
 	git tag -a "${new_version}"
 	git push origin HEAD
@@ -60,33 +76,43 @@ if [[ "${answer}" = "y" ]] ; then
 fi
 answer=
 
+update_aur_pkg(){
+	aur_pkg_name="$1"
+	pkgbuild_name="$2"
+
+	aur_dev_repo_dir="${aur_dev_repo_root}/${aur_pkg_name}"
+
+	echo
+	echo "***************************************"
+	echo "**    Updating ${aur_pkg_name} AUR PKBUILD...    **"
+	echo "***************************************"
+	echo
+	./maintenance_scripts/changelog.sh > "${aur_dev_repo_dir}"/CHANGELOG
+	cp ./packaging/arch/"$pkgbuild_name" "${aur_dev_repo_dir}"/PKGBUILD
+	cd "${aur_dev_repo_dir}"
+	makepkg --printsrcinfo > .SRCINFO
 
 
-echo
-echo "***************************************"
-echo "**    Updating AUR dev PKBUILD...    **"
-echo "***************************************"
-echo
-./maintenance_scripts/changelog.sh > "${aur_dev_repo_dir}"/CHANGELOG
-cp ./packaging/arch/PKGBUILD "${aur_dev_repo_dir}"/PKGBUILD
-cd "${aur_dev_repo_dir}"
-makepkg --printsrcinfo > .SRCINFO
+	echo
+	echo "??????????????????????????????????????????????????"
+	echo "??    Confirm push to ${aur_pkg_name} AUR? [y/N]    ??"
+	echo "??????????????????????????????????????????????????"
+	echo -n "> "
+	read -r answer
+	echo
+	if [[ "${answer}" = "y" ]] ; then
+		git add PKGBUILD .SRCINFO CHANGELOG
+		git commit -m "update to ${new_version}"
+		git push origin HEAD
+	fi
+	answer=
 
+}
 
-echo
-echo "??????????????????????????????????????????????????"
-echo "??    Confirm push to AUR dev package? [y/N]    ??"
-echo "??????????????????????????????????????????????????"
-echo -n "> "
-read -r answer
-echo
-if [[ "${answer}" = "y" ]] ; then
-	git add PKGBUILD .SRCINFO CHANGELOG
-	git commit -m "update to ${new_version}"
-	git push origin HEAD
-fi
-answer=
-
+update_aur_pkg oomox-git PKGBUILD_migration
+update_aur_pkg themix-full-git PKGBUILD_full_git
+update_aur_pkg themix-gui-git PKGBUILD_gui
+update_aur_pkg themix-import-images PKGBUILD_import_image
 
 echo
 echo '$$$$$$$$$$$$$$$$$$$$$$$$$'
