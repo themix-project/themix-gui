1422a05d9ea16df39c507e6df7b7175d35801abe
actionless <actionless.loveless@gmail.com>
2020-12-25 19:42:18 +0100

    chore(maintenance_scripts: git-status): add more exceptions

--- maintenance_scripts/git-status.sh
+++ maintenance_scripts/git-status.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-git -c 'color.status=always' status | grep -v -e colors/ -e dumpster/ -e '\[31mgtk-theme\[m' -e 'env.*/' -e '[^/]*png' -e 'git-status'
+git -c 'color.status=always' status \
+    | grep -v \
+        -e colors/ \
+        -e dumpster/ \
+        -e '\[31mgtk-theme\[m' \
+        -e 'env.*/' \
+        -e '[^/]*png' \
+        -e '[^/]git-status' \
+        -e 'misc' \
+        -e 'modified:   plugins/.* (untracked content)'
