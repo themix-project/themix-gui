15b1befaea4d970f4066f9a8b49e8d63f991df2d
actionless <actionless.loveless@gmail.com>
2022-10-12 18:33:32 +0200

    chore(maintenance_scripts: recent-shitory): nicer output

--- maintenance_scripts/show_recent_history.sh
+++ maintenance_scripts/show_recent_history.sh
@@ -3,14 +3,31 @@ set -euo pipefail
 
 filter="cat"
 if [[ "${1:-}" = '-c' ]] ; then
-	filter="grep -v -i -E -e (typing|typehint|coverage|github|docker) -e actionless.*(chore|test|style|doc|Revert|Merge|readme|refactor).*:"
+	filter="grep -v -i -E \
+		-e (typing|typehint|coverage|github|docker|vulture) \
+		-e actionless\s[^[:print:]]\[m(doc|chore|test|style|Revert|Merge|refactor)\
+		-e [^[:print:]]\[31m[[:print:]]+[^[:print:]]\[m
+	"
 	shift
 fi
 
-git log \
+result=$(git log \
 	--pretty=tformat:"%Cred%D%Creset %ad %Cgreen%h %Cblue%an %Creset%s" \
 	--date='format:%Y-%m-%d' \
 	--color=always \
 	"$(git tag | grep -v gtk | sort -V | tail -n1)"~1.. \
 	"$@" \
-| $filter
+)
+
+if [[ "${filter}" = "cat" ]] ; then
+	echo "$result"
+else
+	echo "Notable changes:"
+	echo "$result" | $filter
+	echo
+	echo "Submodule changes:"
+	grep submodule <<< "$result"
+	echo
+	echo "Previous release:"
+	echo "$result" | tail -n1
+fi
