9843e82ead2dfad31ddc118c40757d7566ef61dd
actionless <actionless.loveless@gmail.com>
2022-12-03 06:43:39 +0100

    style(palette_cache): mypy

--- oomox_gui/palette_cache.py
+++ oomox_gui/palette_cache.py
@@ -1,43 +1,54 @@
 import json
 
+from gi.repository import Gdk
+
 from .config import USER_PALETTE_PATH, DEFAULT_ENCODING
+
+
+PaletteCacheT = list[str]
 
 
 class PaletteCache():
 
-    _palette_cache = None
+    _palette_cache: PaletteCacheT | None = None
 
     @staticmethod
-    def load():
+    def load() -> PaletteCacheT:
         try:
             with open(USER_PALETTE_PATH, 'r', encoding=DEFAULT_ENCODING) as file_object:
-                return json.load(file_object)
-        except FileNotFoundError:
+                result = json.load(file_object)
+            if not isinstance(result, list):
+                raise RuntimeError("Error loading palette cache")
+            for item in result:
+                if not isinstance(item, str):
+                    raise RuntimeError("Error loading palette cache")
+            return result
+        except (FileNotFoundError, RuntimeError):
             return []
 
     @staticmethod
-    def save(palette_cache_list):
+    def save(palette_cache_list: PaletteCacheT) -> None:
         with open(USER_PALETTE_PATH, 'w', encoding=DEFAULT_ENCODING) as file_object:
             return json.dump(palette_cache_list, file_object)
 
     @classmethod
-    def set(cls, palette_cache_list):
+    def set(cls, palette_cache_list: PaletteCacheT) -> None:
         cls._palette_cache = palette_cache_list
         cls.save(palette_cache_list)
 
     @classmethod
-    def get(cls):
+    def get(cls) -> PaletteCacheT:
         if not cls._palette_cache:
             cls._palette_cache = cls.load()
         return cls._palette_cache
 
     @classmethod
-    def get_gtk(cls):
+    def get_gtk(cls) -> str:
         return ':'.join(cls.get())
 
     @classmethod
-    def add_color(cls, gtk_color):
-        gtk_color_converted = gtk_color.to_color().to_string()
+    def add_color(cls, gtk_color: Gdk.RGBA) -> None:
+        gtk_color_converted = gtk_color.to_color().to_string()  # type: ignore[func-returns-value]
         palette_cache_list = [
             string for string in cls.get()  # pylint: disable=not-an-iterable
             if string != ''
